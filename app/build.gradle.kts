0a43a3f4039d71a5e05c4347cb8b0001fa1556cf
Suresh De Zoysa <gsndezoysa@gmail.com>
2026-05-07 15:51:59 +0530

    chore: bump version to 1.0.2 for closed testing release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId         = "com.daqmobile.imulink"
         minSdk                = 26
         targetSdk             = 35
-        versionCode           = 1
-        versionName           = "1.0.0"
+        versionCode           = 3
+        versionName           = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
